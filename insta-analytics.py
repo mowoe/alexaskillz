179bc4d4cac3c7677fc2f40af2eee0ba83509e0d
mowoe <github@mowoe.com>
2018-11-16 16:08:14 +0100

    bugfix

--- insta-analytics.py
+++ insta-analytics.py
@@ -21,37 +21,32 @@ heads = {"Accept":"*/*",
 @ask.launch
 def launch():
     speech_text = "Hallo! Frag einfach 'Wie viele Abonennten hat der Benutzer'"
-    return question(speech_text).simple_card('Hello', speech_text)
-
-@ask.intent('HelloIntent')
-def hello():
-    speech_text = "Hallo"
-    return statement(speech_text).simple_card('Hello', speech_text)
+    return question(speech_text).simple_card('Hallo!', speech_text)
 
 @ask.intent('FollowerIntent')
 def followers(username):
     resp = requests.get("https://api.instastatistics.com/statistics/"+username,headers=heads)
     if resp.json()["success"] == False:
-        return statement("Ein Fehler ist aufgetreten. Tut mir Leid!").simple_card("Ein Fehler ist aufgetreten. Tut mir Leid!")
+        return statement("Ein Fehler ist aufgetreten. Tut mir Leid!").simple_card("Fehler!","Ein Fehler ist aufgetreten. Tut mir Leid!")
     else:
         followers = resp.json()["data"]["followers"]
         speech_text = "Der gewuenschte Benutzername hat {} Abonennten auf Instagram".format(followers)
-        return statement(speech_text).simple_card(speech_text)
+        return statement(speech_text).simple_card("Abonnenten",speech_text)
 
 @ask.intent('AMAZON.HelpIntent')
 def help():
-    speech_text = 'Du kannst Hallo zu mir sagen!'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+    speech_text = "Frag einfach 'Wie viele Abonennten hat der Benutzer'"
+    return question(speech_text).reprompt(speech_text).simple_card('Hilfe', speech_text)
 
 @ask.intent('AMAZON.CancelIntent')
 def cancel():
     speech_text = 'Ok Ciao!'
-    return statement(speech_text)
+    return statement(speech_text).simple_card("Bis Bald", speech_text)
 
 @ask.intent('AMAZON.StopIntent')
 def stop():
     speech_text = 'Ok Ciao!'
-    return statement(speech_text)
+    return statement(speech_text).simple_card("Bis Bald", speech_text)
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1")
